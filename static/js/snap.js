43178a53dd150f6ea7ee3da6fce48355ae67e266
Isak Rathe Stoere <rathe.stoere@gmail.com>
2017-09-21 17:20:15 +0200

    Abort this one, just unable to switch branch.

---
+++ static/js/snap.js
@@ -0,0 +1,8 @@
+$(#takePhoto).on("click", function (numClicked) {
+	// Changing snap photo as a person clicks on takePhoto div
+	var idhide = "snap" + numClicked + "p";
+	var idshow = "snap" + ++numClicked + "p";
+	$(idhide).hide();
+	$(idshow).removeClass(".hide");
+	alert("takePhoto is now clicked " + numClicked + " times.");
+});
