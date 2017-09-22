4f1eb6c5b5485e59caaaa3334bd6113072e57c6f
Marius Maaland <mariusmaaland@gmail.com>
2017-09-22 14:17:34 +0200

    remove snap.css, and tailor snap.js to new general template

--- static/js/snap.js
+++ static/js/snap.js
@@ -1,12 +1,26 @@
 $("#menuItemSnap").on("click", function () {
 	// show snap opening page
-	var h = $("#iPhone").height();
-	var w = $("#IPhone").width();
+	$(".phone").html(`<div class="phone">
+    <img src="../design/iphone.svg" alt="iPhone" id="iPhone">
+        
+        <!-- snappictures -->
+        <img src="img/snap/snapopen.png" alt="snapopen" id="snapopen" class="hide placeInPhone">
+        <img src="img/snap/snap0p.png" alt="snap0p" id="snap0p" class="hide placeInPhone">
+        <img src="img/snap/snap1p.png" alt="snap1p" id="snap1p" class="hide placeInPhone">
+        <img src="img/snap/snap2p.png" alt="snap2p" id="snap2p" class="hide placeInPhone">
+        <img src="img/snap/snap3p.png" alt="snap3p" id="snap3p" class="hide placeInPhone">
+
+</div>
+`)
 	$("#snapopen").removeClass("hide");
+	$(".app_title").text("Snapchat");
+	$("#info span").text("Snapchat gir oss mye glede i hverdagen");
+	$("#btc_price").hide();
 
 	// show text info
-	$("#infoHeader").text("Snap gir oss mye glede i hverdagen.")
+	// $("#infoHeader").text("Snap gir oss mye glede i hverdagen.")
 })
+
 
 var numClicked = 0;
 $(".placeInPhone").on("click", function () {
