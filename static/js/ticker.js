fed9d55bfb58b8ed827c5fb2c3c682cefea085ef
Marius Maaland <mariusmaaland@gmail.com>
2017-09-22 12:00:24 +0200

    add iphone flexbox container with the iphone image and screenshot image inside

---
+++ static/js/ticker.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	// show snap opening page
+	var h = $("#iPhone").height();
+	var w = $("#IPhone").width();
+	$("#btc_img").removeClass("hide");
+	$("#btc_img").css("width", w)
+	$("#btc_img").css("height", h)
+
+
+
+	// show text info
+	// $("#info").text("lorem ipsum bitcoin")
+})
