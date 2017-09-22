b17014d002188031d860667e0a1bca4a6cb1a2aa
johannes <johannes.z.vollan@gmail.com>
2017-09-22 14:52:12 +0200

    add serv static files

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 //entry point for server
 let port = 8082
 import express from 'express'
+import path from 'path'
+
 let app = express();
+
+//serving the main page
+app.use(express.static(__dirname + '/../static'));
+app.get('/', (req, res)=>{
+ res.sendFile(path.join(__dirname+'/../static/index.html'));
+})
 app.listen(port,()=> console.log(`listening on port: ${port}`) )
