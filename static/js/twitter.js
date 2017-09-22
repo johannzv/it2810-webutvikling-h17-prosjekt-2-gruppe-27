bc5e4fa7516ae521b4dbbbf5cf1c25d23ad7b04c
johannes <johannes.z.vollan@gmail.com>
2017-09-22 20:14:07 +0200

    add fill twitter feed with trump tweets

--- static/js/twitter.js
+++ static/js/twitter.js
@@ -1,3 +1,4 @@
+$("#refreshTwitterFeed").toggle();
 $(".twitter-buttons").on("mouseenter", function() {
   $(this).css({
     "background-color": "#ADADAD",
@@ -6,3 +7,28 @@ $(".twitter-buttons").on("mouseenter", function() {
 }).on("mouseleave", function() {
   $(this).css("background-color", "white")
 })
+
+$("#getTwitterFeed, #refreshTwitterFeed").click(function() {
+  if (this.id== "getTwitterFeed"){
+    $("#getTwitterFeed").toggle();
+    $("#refreshTwitterFeed").toggle();
+  }
+  $("#twitterFeed").empty();
+  $.getJSON("/api/twitter", function(data, status) {
+    var tweets = data.data
+    $.each(tweets, function(index, value){
+      $("#twitterFeed").append(`
+        <li class="twitterItem" id="tweetItem${index}">
+        <img src="img/president.svg" width="50px" height="50px" />
+        <div>
+          <h5> <span>Donald J. Trump </span> @realDonaldTrump </h5>
+          <p>
+          ${value}
+          </p>
+        </div>
+      </li>`)
+    })
+
+
+  })
+});
