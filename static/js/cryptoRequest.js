0f54b6aea723715bf60850dcbc603129f673f4bb
Marius Maaland <mariusmaaland@gmail.com>
2017-09-22 10:41:40 +0200

    add setTimeout to ajax function to poll api every 5 seconds

--- static/js/cryptoRequest.js
+++ static/js/cryptoRequest.js
@@ -1,10 +1,18 @@
-$(document).ready((function() {
-  var poloniexAPI = "https://poloniex.com/public?command=returnTicker";
-  $.getJSON( poloniexAPI, function( data ) {
-    if ( data != "ERROR" ) {
-    	price = Number(data.USDT_BTC.last)
+function getPrice() {
+  $.ajax({
+    url: "https://poloniex.com/public?command=returnTicker",
+    success: function(data) {
+      if ( data != "ERROR" ) {
+    	price = parseInt(data.USDT_BTC.last)
     }
-	})
-}));
+    $("#btc_price span").text(price+" USD");
+    }
+  });
+  setTimeout(getPrice, 5000); 
+}
 
-
+$(document).ready(function() {
+  // run the first time; all subsequent calls will take care of themselves every 5 seconds
+  getPrice()
+  setTimeout(getPrice, 5000);
+});
