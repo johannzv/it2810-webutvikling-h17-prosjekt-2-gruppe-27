dd786e8ce0fdec4b2d1b1554df60750b26a74546
Marius Maaland <mariusmaaland@gmail.com>
2017-09-23 11:37:23 +0200

    add logic for green and red price text, but it is not working

--- static/js/cryptoRequest.js
+++ static/js/cryptoRequest.js
@@ -1,20 +1,36 @@
-function getPrice() {
+function getPrice(prev) {
   $.ajax({
     url: "https://poloniex.com/public?command=returnTicker",
     success: function(data) {
       if ( data != "ERROR" ) {
     	price = parseInt(data.USDT_BTC.last)
     }
+    console.log("prev");
+    console.log(prev);
+    console.log("price");
+    console.log(price);
+    if (prev != -1) {
+      if (price > prev) {
+        console.log("In higher");
+        $("btc_price span").css('color', 'green');
+      } else if (price < prev) {
+        console.log("In lower");
+        $("btc_price span").css('color', 'red');
+      } else {
+        console.log("In either");
+        $("btc_price span").css('color', 'black');
+      }
+    }
     $("#btc_price span").fadeOut();
     $("#btc_price span").text(price+" USD");
     $("#btc_price span").fadeIn();
+    setTimeout(getPrice, 5000, price);
     }
   });
-  setTimeout(getPrice, 5000); 
+   
 }
 
 $(document).ready(function() {
   // run the first time; all subsequent calls will take care of themselves every 5 seconds
-  getPrice();
-  // setTimeout(getPrice, 5000);
+  getPrice(-1);
 });
