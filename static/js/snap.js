1d8df30f906989cead85471c81321acfbaedb9c7
Marius Maaland <mariusmaaland@gmail.com>
2017-09-24 19:09:42 +0200

    generalize loading of html

--- static/js/snap.js
+++ static/js/snap.js
@@ -1,23 +1,14 @@
 $("#menuItemSnap").on("click", function () {
 	
 	// show snap opening page
-	$(".phone").html(`
-    <img src="img/snap/snap0p.png" alt="iPhone" id="iPhone">
-        <!-- snappictures -->
-        <img src="img/snap/snap0p.png" alt="snap0p" id="snap0p" class="hide placeInPhone">
-        <img src="img/snap/snap1p.png" alt="snap1p" id="snap1p" class="hide placeInPhone">
-        <img src="img/snap/snap2p.png" alt="snap2p" id="snap2p" class="hide placeInPhone">
-        <img src="img/snap/snap3p.png" alt="snap3p" id="snap3p" class="hide placeInPhone">
-`)
-	$(".app_title").hide().text("Snapchat").slideDown();
-	$("#info").hide().text("Snapchat gir oss mye glede i hverdagen").fadeIn();
-	$(".btc_price_container").hide();
-	$("#snap_bar").show();
-	$("#crypto_bar").hide();
-	$("#twitter_bar").hide();
-
-	// show text info
-	// $("#infoHeader").text("Snap gir oss mye glede i hverdagen.")
+	$(".content").load('snap.html', function () {
+		$(".app_title").hide().slideDown();
+		$("#info").hide().fadeIn();
+		$(".btc_price_container").hide();
+		$("#snap_bar").show();
+		$("#crypto_bar").hide();
+		$("#twitter_bar").hide();
+	})
 })
 
 
--- static/js/ticker.js
+++ static/js/ticker.js
@@ -1,14 +1,17 @@
 
 $("#menuItemCrypto").on("click", function () {
 	// show snap opening page
-	$(".phone").html(`
-    <img src="img/btc.png" alt="iPhone" id="iPhone">`);
-	$("#btc_image").removeClass("hide");
-	$(".app_title").hide().text("Bitcoin Ticker").slideDown();
-	$("#info").hide().text("Bitcoin Ticker lar deg sjekke oppdaterte priser på kryptovaluta som Bitcoin, Ethereum, Litecoin osv., samt få push-varsler ved endringer.").fadeIn();
-	$(".btc_price_container").slideDown();
-	$("#crypto_bar").show();
-	$("#snap_bar").hide();
-	$("#twitter_bar").hide();
+	$(".content").load('ticker.html', function() {
+		$("#tickerImg").removeClass("hide");
+		$(".app_title").hide().slideDown();
+		$("#info").hide().fadeIn();
+		$(".btc_price_container").slideDown();
+		$("#crypto_bar").show();
+		$("#snap_bar").hide();
+		$("#twitter_bar").hide();
+	});
+
+		
+
 
 })
